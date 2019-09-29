e02b4f9dfe5d1e3da432d9ecfca581dcf9fb8537
Louis Dalligos <zindex14@gmail.com>
2019-09-29 16:09:35 +0800

    Add private route

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
+// Route
+import PrivateRoute from "Routes/PrivateRoute";
+
 // Context API
 import AuthState from "Context/auth/AuthState";
 import AlertState from "Context/alerts/AlertState";
+import TherapyState from "Context/therapy/TherapyState";
 
 // Pages
 import Dashboard from "Pages/Dashboard/Dashboard";
-import RegisterPage from "Pages/Register";
+//import RegisterPage from "Pages/Register";
 
 // Components
 import LoginForm from "Components/Forms/LoginForm/LoginForm";
-//import AccountRegistrationForm from "Components/Forms/AccountRegistrationForm/AccountRegistrationForm";
+import AccountRegistrationForm from "Components/Forms/AccountRegistrationForm/AccountRegistrationForm";
 
-import setAuthToken from "Utils/setAuthToken";
 // load token into global headers
+import setAuthToken from "Utils/setAuthToken";
+
 if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
@@ -26,15 +31,21 @@ const history = createBrowserHistory();
 const App = props => {
   return (
     <AuthState>
-      <AlertState>
-        <Router history={history}>
-          <Switch>
-            <Route exact path={"/"} component={Dashboard} />
-            <Route exact path={"/register"} component={RegisterPage} />
-            <Route exact path={"/login"} component={LoginForm} />
-          </Switch>
-        </Router>
-      </AlertState>
+      <TherapyState>
+        <AlertState>
+          <Router history={history}>
+            <Switch>
+              <PrivateRoute exact path={"/"} component={Dashboard} />
+              <Route
+                exact
+                path={"/register"}
+                component={AccountRegistrationForm}
+              />
+              <Route exact path={"/login"} component={LoginForm} />
+            </Switch>
+          </Router>
+        </AlertState>
+      </TherapyState>
     </AuthState>
   );
 };
