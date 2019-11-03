a9fc1dcd37de8b29c084df7f4edf767bb1527a2f
Louis Dalligos <zindex14@gmail.com>
2019-11-03 15:18:52 +0800

    Complete our logic for leaving a route - show confirm

--- src/components/utility-components/RouteLeavingGuard.js
+++ src/components/utility-components/RouteLeavingGuard.js
@@ -1,68 +1,40 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Prompt } from "react-router-dom";
 import { Modal } from "antd";
 
 const RouteLeavingGuard = ({ shouldBlockNavigation, navigate, when }) => {
-  const [visible, setVisible] = useState(false);
   const [lastLocation, setlastLocation] = useState(null);
   const [confirmedNavigation, setConfirmedNavigation] = useState(false);
 
   useEffect(() => {
-    if (confirmedNavigation && !visible) {
-      console.log(lastLocation.pathname);
+    // check if user confirmed, then set the pathname to prop on the form
+    if (confirmedNavigation) {
       navigate(lastLocation.pathname);
     }
-  }, [confirmedNavigation]);
-
-  const showModal = location => {
-    setVisible(true);
-    setlastLocation(location);
-  };
-
-  const closeModal = (callback, ...params) => {
-    setVisible(false);
-    callback(params);
-  };
+  }, [lastLocation, navigate, confirmedNavigation]);
 
   const handleBlockedNavigation = nextLocation => {
-    console.log(nextLocation, "handle blocked navigation");
     if (!confirmedNavigation && shouldBlockNavigation(nextLocation)) {
-      showModal(nextLocation);
+      setlastLocation(nextLocation);
+      Modal.confirm({
+        title: "Are you sure?",
+        content: "Changes will not be saved",
+        okText: "Yes, proceed",
+        cancelText: "Continue editing",
+        onOk() {
+          setConfirmedNavigation(true);
+        },
+        onCancel() {
+          console.log("Cancel");
+        }
+      });
       return false;
     }
 
     return true;
   };
 
-  const handleConfirmNavigationClick = () =>
-    closeModal(lastLocation => {
-      console.log(lastLocation);
-      if (lastLocation) {
-        setConfirmedNavigation(true);
-        // this.setState({
-        //    confirmedNavigation: true
-        // }, () => {
-        //    // Navigate to the previous blocked location with your navigate function
-        //    navigate(lastLocation.pathname)
-        // })
-      }
-    });
-
-  return (
-    <Fragment>
-      <Prompt when={when} message={handleBlockedNavigation} />
-      <Modal
-        title="Are you sure?"
-        visible={visible}
-        onOk={handleConfirmNavigationClick}
-        onCancel={closeModal}
-        okText="Yes, proceed"
-        cancelText="Continue editing"
-      >
-        <p>Changes will not be saved</p>
-      </Modal>
-    </Fragment>
-  );
+  return <Prompt when={when} message={handleBlockedNavigation} />;
 };
 
 export default RouteLeavingGuard;
